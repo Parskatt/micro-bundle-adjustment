b6f8271996c7cb8179df0dbc5d81b07390921930
Johan Edstedt <johan.edstedt@liu.se>
2024-01-05 17:05:50 +0100

    wip

--- demo.py
+++ demo.py
@@ -1,54 +1,62 @@
 import torch
-from kornia.geometry import axis_angle_to_rotation_matrix
-from micro_bundle_adjustment import bundle_adjust
+from torch.func import vmap
+from kornia.geometry import axis_angle_to_rotation_matrix, relative_camera_motion, rotation_matrix_to_axis_angle
+from micro_bundle_adjustment import lm_optimize
 
 
 def projection(X, r, t):
-    if len(X.shape) == 1: # should be unlikely
-        X = X[None]
     R = axis_angle_to_rotation_matrix(r[None])[0]
-    x = (R @ X.mT).mT + t[None]
+    if len(X.shape) > 1:#TODO: don't want this
+        x = (R @ X.mT).mT + t[None]
+    else:        
+        x = (R @ X) + t
     return x[...,:2]/x[...,[2]]
 
-def gold_standard_residuals(X, theta, oberservations):
-    M = len(oberservations)
-    r, t = theta.chunk(2,dim=1)
-    residuals = []
-    for im in range(len(observations)):
-        x_im, inds_im = observations[im]
-        r_im = x_im - projection(X[inds_im], r[im], t[im])
-        residuals.append(r_im)
-    return torch.cat(residuals, dim=1)
+def gold_standard_residuals(X, theta, x_im):
+    r, t = theta.chunk(2)
+    r_im = x_im - projection(X, r, t)
+    return r_im
 
 if __name__ == "__main__":
     N = 100_000
-    dtype = torch.float32
+    dtype = torch.float64
     device = "cuda"
-    X = torch.rand(N, 3).to(device=device,dtype=dtype)
+    X = torch.randn(N, 3).to(device=device,dtype=dtype)
     X[...,2] = X[...,2] + 10
     
-    r = torch.rand(2,3).to(device=device,dtype=dtype) * 1 # A large rotation
-    t = torch.rand(2,3).to(device=device,dtype=dtype) * 0.5 # A small translation
+    r = torch.randn(2,3).clamp(-0.5,0.5).to(device=device,dtype=dtype) * 1 # A large rotation
+    t = torch.randn(2,3).clamp(-1,1).to(device=device,dtype=dtype) * 0.5 # A small translation
     
-    x_a = projection(X, r[0], t[0])
-    x_b = projection(X, r[1], t[1])
+    batch_projection = vmap(projection, in_dims=(0, None, None))
+    x_a = batch_projection(X, r[0], t[0])
+    x_b = batch_projection(X, r[1], t[1])
 
-    observations = [(x_a + 0.001*torch.rand_like(x_a), torch.arange(len(X), device = device)[:,None])] + \
-        [(x_b + 0.001*torch.rand_like(x_b), torch.arange(len(X), device = device)[:,None])]
-    X_0 = X + 0.1*torch.rand_like(X)
-    noisy_r = r + torch.rand_like(r)*0.1
-    noisy_t = t + torch.rand_like(t)*0.1
+    observations = [(x_a, torch.arange(len(X), device = device))] + \
+        [(x_b, torch.arange(len(X), device = device))]
+    X_0 = X + torch.randn_like(X).clamp(-1,1)*0.05
+    noisy_r = r + torch.randn_like(r).clamp(-1,1)*0.01
+    noisy_t = t + torch.randn_like(t).clamp(-1,1)*0.5
     theta_0 = torch.cat((noisy_r, noisy_t), dim = -1)
     
-    X_hat, theta_hat = bundle_adjust(gold_standard_residuals, X_0, theta_0, observations)
-    r_hat, t_hat = theta_hat.chunk(2)
-    r_error_opt = (r_hat-r).norm()
-    r_error_init = (noisy_r-r).norm()
+    with torch.no_grad():
+        X_hat, theta_hat = lm_optimize(gold_standard_residuals, X_0, theta_0, observations, dtype=dtype, L_0 = 1e-2, num_steps = 20)
+        
+    R = axis_angle_to_rotation_matrix(r)
+    R_rel, t_rel = relative_camera_motion(R[:1], t[:1,:,None], R[1:], t[1:,:,None])
+    r_rel = rotation_matrix_to_axis_angle(R_rel)
     
-    t_error_opt = (t_hat-t).norm()
-    t_error_init = (noisy_t-t).norm()
+    r_hat, t_hat = theta_hat.chunk(2, dim=1)
+    R_hat = axis_angle_to_rotation_matrix(r_hat)
+    R_hat_rel, t_hat_rel = relative_camera_motion(R_hat[:1], t_hat[:1, :, None], R_hat[1:], t_hat[1:, :, None])
+    r_hat_rel = rotation_matrix_to_axis_angle(R_hat_rel)
+    
+    r_error_opt = (r_hat_rel-r_rel).norm()
+    
+    t_error_opt = (t_hat_rel-t_rel).norm()
+    
+    X = (X-X.mean(dim=0))
+    X_hat = (X_hat-X_hat.mean(dim=0))
     
     X_error_opt = (X-X_hat).norm(dim=-1).mean()
-    X_error_init = (X_0-X).norm(dim=-1).mean()
     
-    print(f"Residuals: {r_error_init=} {r_error_opt=} {t_error_init=} {t_error_opt=} {X_error_init=} {X_error_opt=}")
+    print(f"Residuals: {r_error_opt=} {t_error_opt=} {X_error_opt=}")
--- micro_bundle_adjustment/bundle_adjustment.py
+++
@@ -1,9 +0,0 @@
-import torch
-from .optimizer import lm_optimize
-from typing import Callable
-
-def bundle_adjust(residual_function: Callable, X_0: torch.Tensor, theta_0: torch.Tensor, observations: list[tuple[torch.Tensor, torch.Tensor]]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-    N, D  = X_0.shape
-    with torch.no_grad():
-        X, theta = lm_optimize(residual_function, X_0, theta_0, observations, num_steps = 10)
-    return X, theta
